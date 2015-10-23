8691d703f632864268f583082beb6cdfa48b244d
Palumbo <p@lumbo.me>
2015-10-23 07:29:25 -0500

    added the resize example

---
+++ resize/script.js
@@ -0,0 +1,27 @@
+function adjustSyle() { 
+	var width = 0; 
+	//get width..more cross browser issues
+	if (window.innerHeight) { 
+		width = window.innerWidth; 
+	} else if (document.documentElement && document.documentElement.clientHeight) { 
+		width = document.documentElement.clientWidth; 
+	} else if (document.body) { 
+		width = document.body.clientWidth; 
+	}
+	
+	// now we should have it
+	if (width < 600) { 
+		document.getElementById('myCSS').setAttribute("href", "_css/narrow.css"); 
+	} else { 
+		document.getElementById('myCSS').setAttribute('href', '_css/main.css'); 
+	}
+}
+
+// now call it when the window is resized
+window.onresize = function() { 
+	adjustSyle()
+}; 
+
+window.onload = function() { 
+	adjustSyle()
+}; 
